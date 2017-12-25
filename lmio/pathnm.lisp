f477210bdc6086105a37c189023fc24dd7228998
Alfred M. Szmidt <ams@gnu.org>
2017-12-25 18:07:55 +0100

    lmpat/system-78-22.lisp: Fix READ-SYNTAX-PLIST's lossage with :LINE-IN on binary files.

--- lmio/pathnm.lisp
+++ lmio/pathnm.lisp
@@ -2484,11 +2484,13 @@ Type Resume to retry.~]"
   (SETQ WO (FUNCALL STREAM ':WHICH-OPERATIONS))
   (COND ((MEMQ ':SYNTAX-PLIST WO)
 	 (SETQ PLIST (FUNCALL STREAM ':SYNTAX-PLIST)))
-	((AND (MEMQ ':READ-INPUT-BUFFER WO)
-	      (MULTIPLE-VALUE-BIND (BUFFER START END)
-		  (FUNCALL STREAM ':READ-INPUT-BUFFER)
-		(AND BUFFER (SETQ PLIST (FILE-PARSE-PROPERTY-LIST BUFFER START END)))
-		PLIST)))			;Try :LINE-IN if this fails
+	;; This way of reading the property list is sort of a kludge, but it has
+	;; the very important advantage that it won't ever read the whole file like :LINE-IN
+	;; would be prone to do if the file had no carriage returns in it.
+	((MEMQ ':READ-INPUT-BUFFER WO)
+	 (MULTIPLE-VALUE-BIND (BUFFER START END)
+	     (FUNCALL STREAM ':READ-INPUT-BUFFER)
+	   (AND BUFFER (SETQ PLIST (FILE-PARSE-PROPERTY-LIST BUFFER START END)))))
 	(T (DO ((LINE) (EOF)) (NIL)
 	     (MULTIPLE-VALUE (LINE EOF) (FUNCALL STREAM ':LINE-IN NIL))
 	     (COND ((STRING-SEARCH "-*-" LINE)
